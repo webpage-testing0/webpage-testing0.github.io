c6a8029150dbbbe334592d2ec251014d66d2706c
webpage-testing0 <elisona.krasniqi@icloud.com>
2025-06-02 18:34:31 +0200

    Add files via upload

---
+++ js/vendor/woocommerce/js/tpl-woocommerce.js
@@ -0,0 +1,18 @@
+var wc_single_product_params = {
+    "i18n_required_rating_text":"Please select a rating",
+    "review_rating_required":"yes",
+    "flexslider":{
+        "rtl":false,
+        "animation":"slide",
+        "smoothHeight":false,
+        "directionNav":false,
+        "controlNav":"thumbnails",
+        "slideshow":false,
+        "animationSpeed":500,
+        "animationLoop":false
+    },
+    "zoom_enabled":"1",
+    "photoswipe_enabled":"1",
+    "flexslider_enabled":"1"
+};
+
